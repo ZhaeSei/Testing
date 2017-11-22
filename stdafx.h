3775a0ab90c3ccb0afe81fa73554e41083e73026
Zhae Sei <33882778+ZhaeSei@users.noreply.github.com>
2017-11-21 18:51:04 -0700

    Add files via upload

--- stdafx.h
+++ stdafx.h
@@ -4,6 +4,7 @@
 //
 
 #pragma once
+
 #include <iostream>
 
 #include <string>
@@ -11,8 +12,21 @@
 #include "targetver.h"
 
 #include <stdio.h>
-#include <tchar.h>
 
+
+
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
 
 
 // TODO: reference additional headers your program requires here
